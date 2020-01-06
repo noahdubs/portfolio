6175f6c24982ab115c42871391bdc5065cc48c77
noahdubs <artaxnoah@gmail.com>
2020-01-05 20:20:25 -0600

    add responsivness to tech used

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -21,12 +21,22 @@ const Projects = () => {
                                 </div>
                             </div>
                             <p className="card-body">A workout based site where users can create their own workouts and exercises</p>
-                            <div className="tech-used">
-                                <p className="tech-list">Tech used:</p>
-                                <li className="tech-list">React <i className="fab fa-react"></i></li>
-                                <li className="tech-list">Node.js <i className="fab fa-node-js"></i></li>
-                                <li className="tech-list">Express.js <i className="fab fa-js-square"></i></li>
-                                <li className="tech-list">MongoDB <i className="fas fa-database"></i></li>
+                            <div className="row tech-used">
+                                <div className="col-sm-12">
+                                    <p className="tech-list">Tech used:</p>
+                                </div>
+                                <div className="col-sm-6 col-md-3">
+                                    <li className="tech-list">React <i className="fab fa-react"></i></li>
+                                </div>
+                                <div className="col-sm-6 col-md-3">
+                                    <li className="tech-list">Node.js <i className="fab fa-node-js"></i></li>
+                                </div>
+                                <div className="col-sm-6 col-md-3">
+                                    <li className="tech-list">Express.js <i className="fab fa-js-square"></i></li>
+                                </div>
+                                <div className="col-sm-6 col-md-3">
+                                    <li className="tech-list">MongoDB <i className="fas fa-database"></i></li>    
+                                </div>
                             </div>
                             <a href="http://ec2-18-220-6-113.us-east-2.compute.amazonaws.com/" className="btn btn-view"><i className="fas fa-desktop"></i> View live</a>
                             <a href="https://github.com/noahdubs/workout" className="btn btn-view"><i className="fab fa-github"></i> View code</a>
@@ -49,11 +59,19 @@ const Projects = () => {
                                 </div>
                             </div>
                             <p className="card-body">A blog site where users can make blog posts and leave comments on posts</p>
-                            <div className="tech-used">
-                                <p className="tech-list">Tech used:</p>
-                                <li className="tech-list">Node.js <i className="fab fa-node-js"></i></li>
-                                <li className="tech-list">Express.js <i className="fab fa-js-square"></i></li>
-                                <li className="tech-list">MongoDB <i className="fas fa-database"></i></li>
+                            <div className="row tech-used">
+                                <div className="col-sm-12">
+                                    <p className="tech-list">Tech used:</p>
+                                </div>
+                                <div className="col-sm-4">
+                                    <li className="tech-list">Node.js <i className="fab fa-node-js"></i></li>
+                                </div>
+                                <div className="col-sm-4">
+                                    <li className="tech-list">Express.js <i className="fab fa-js-square"></i></li>
+                                </div>
+                                <div className="col-sm-4">
+                                    <li className="tech-list">MongoDB <i className="fas fa-database"></i></li>
+                                </div>
                             </div>
                             <a href="http://ec2-3-19-30-151.us-east-2.compute.amazonaws.com/" className="btn btn-view"><i className="fas fa-desktop"></i> View live</a>
                             <a href="https://github.com/noahdubs/blacktop-blog" className="btn btn-view"><i className="fab fa-github"></i> View code</a>
